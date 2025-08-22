1f1e67e0b559d0f555139c80184cf05e6f72ba07
SAAHIL-5 <tomarsaahil@gmail.com>
2025-08-22 16:01:29 +0530

    update server.js

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import {inngest,functions} from './inngest/index.js'
 import { serve } from "inngest/express";
 import { clerkMiddleware } from '@clerk/express'
 import userRouter from './routes/userRoutes.js';
+import serverless from "serverless-http";
 
 const app = express();
 
@@ -19,6 +20,13 @@ app.get('/', (req, res)=> res.send('Server is running'))
 app.use("/api/inngest", serve({ client: inngest, functions }))
 app.use('/api/user', userRouter)
 
-const PORT = process.env.PORT || 4000;
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 4000;
+  app.listen(PORT, () => {
+    console.log(`Server running locally on http://localhost:${PORT}`);
+  });
+}
 
-app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}` ))
+
+export const handler = serverless(app);
+export default app;
